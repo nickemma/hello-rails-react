f007f2847a6a5fe565e34afa3a9451aac6024979
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-11-21 12:41:34 +0100

    fixed rubocop error

--- app/javascript/redux/greetings/greetings.js
+++ app/javascript/redux/greetings/greetings.js
@@ -9,7 +9,7 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'FETCH_GREET/fulfilled':
+    case `${FETCH_GREET}/fulfilled`:
       return { ...state, greeting: action.payload };
     default:
       return state;
